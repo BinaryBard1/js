d82e860493f93ede876f973ed5c850a0b830811e
Aniruddha Shrikhande <124452036+BinaryBard1@users.noreply.github.com>
2024-05-19 09:13:42 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,27 @@
+/*Info
+1. original documentation- tc39.es (ecmascript) -- standards but use mdn by mozilla as it is better readable
+*/
+
+
+"use strict"; // treat all js code as newer version(Abhi automatically strict mode par chalta hai)
+
+// alert("hello") we are using nodejs not browser (browser pe kaam karega idhar nhi)
+
+console.log(3+
+    3
+)// should not be like this as code readability should be high
+
+//number 2 to power 53
+/*
+bigint
+string=> ""
+boolean => true/false
+null => standalone value
+undefined => abhi value assign nhi hui hai
+symbol- uniqueness (react)
+
+object
+*/
+
+console.log(typeof null); // returns object
+console.log(typeof undefined); // returns undefined as it is type
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,28 @@
-console.log("Ani")
+/* Shortcuts- 
+1.Comment-  ctrl+/
+2.Print multiple variable -  console.table([accountId,accountEmail,accountPassword,accountCity])
+3.semicolons can be avoided (And should be)
+
+*/
+
+
+const accountId = 3 //change nhi kar paoge
+let accountEmail = "aniruddha@google.com"// Used now a days as it solves block scope and functional scope issues
+var accountPassword = "12345"//not mostly used scope issues
+accountCity = "jaipur" // Can be done (But Should not be used)
+let accountState; // as value is not assigned it is considered undefined 
+
+//accountId = 2 // not allowed as it is declared as constant
+console.log(accountId);
+
+accountEmail = "ani@yahoo.com" // allowed as it is declared as let
+accountPassword = "54321"
+accountCity = "Mumbai"
+console.table([accountId,accountEmail,accountPassword,accountCity]) //prefered way instead of printing one by one
+
+console.log(accountEmail)
+
+console.log("Hi")
+
+
+// In short use only const and let -- not (var)
